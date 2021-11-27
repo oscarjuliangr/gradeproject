2416051d9749b297867b0cc210e919dcfe4b5140
oscarjuliangr <electronico.julian@gmail.com>
2021-11-27 18:52:49 -0500

    area

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,15 @@
+import socketio
 import arnic_dec
 import time
+
+
 start_time = time.time()
 
 print("--- %s seconds ---" % (time.time() - start_time))
 arnic_dec.arn_dec('3764437209')
 
+# create a Socket.IO server
+sio = socketio.Server()
+
+# wrap with a WSGI application
+app = socketio.WSGIApp(sio)
